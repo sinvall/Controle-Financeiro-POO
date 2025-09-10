4e3f45abc633c0bb5e2f70c17750467e34e8044d
sinvall <antoniosinval.neto@gmail.com>
2025-09-10 09:23:57 -0300

    Classe transação atualizada

--- Transacao.py
+++ Transacao.py
@@ -1,20 +1,29 @@
 from datetime import datetime
 
+
 class Transacao:
-    def __init__(self, valor, descricao, tipo):
-        #Tratamento de Exceção
+    def __init__(self, valor, descricao):
         if not isinstance(valor, (int, float)) or valor <= 0:
-            raise ValueError("O valor da transação deve ser um número positivo.")
-        if not descricao.strip():
+            raise ValueError(
+                "O valor da transação deve ser um número positivo.")
+
+        clean_descricao = descricao.strip()
+
+        if not clean_descricao:
             raise ValueError("A descrição da transação não pode estar vazia.")
-        if tipo not in ["Receita", "Despesa"]:
-            raise ValueError("O tipo da transação deve ser 'Receita' ou 'Despesa'.")
-        
+
+        if any(char.isdigit() for char in clean_descricao):
+            raise ValueError("A descrição não pode conter números.")
+
         self.valor = valor
-        self.descricao = descricao
-        self.tipo = tipo  # "Receita" ou "Despesa"
+        self.descricao = clean_descricao  # Atribui a descrição já limpa
         self.data = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
 
     def __str__(self):
-        sinal = "+" if self.tipo == "Receita" else "-"
-        return f"[{self.data}] {self.tipo}: {self.descricao} {sinal}R${self.valor:.2f}"
+        # Método genérico (sobrescrito nas subclasses)
+        return f"[{self.data}] {self.descricao} R${self.valor:.2f}"
+
+    def aplicar(self, saldo_atual):
+        """Polimórfico: cada subclasse implementa sua própria lógica"""
+        raise NotImplementedError(
+            "Este método deve ser implementado nas subclasses.")
